6982472eafa0d4476ec89345fc4f198b75410c75
sanjeeb-gochhayat <sanjeebgochhayat1997@gmail.com>
2025-05-21 12:33:56 +0530

    ..

---
+++ string.js
@@ -0,0 +1,22 @@
+let firstName = "sanjeeb";
+
+console.log(firstName);
+
+let length = firstName.length;
+console.log(length)
+
+//get the specific element
+
+console.log(firstName[4]);
+
+//get the 2nd last element
+
+console.log(firstName[length - 2]);
+
+let myName = "   somu  ";
+console.log(myName.length);
+
+//trim
+let newName = myName.trim();
+console.log(newName)
+console.log(newName.length)
---
+++ variable.js
@@ -0,0 +1,18 @@
+var firstName = "somu";
+console.log(firstName);
+
+//change
+firstName = "sanjeeb";
+console.log(firstName);
+
+//duplicate varible name with var allowed
+var lastName = "Go";
+var lastName = "Goch";
+
+//duplicate varible name with let not allowed
+// let myName = "somu";
+// let myName = "som";
+
+const pi = 3.14;
+// pi = 3.5; -> not possible
+console.log(pi);
